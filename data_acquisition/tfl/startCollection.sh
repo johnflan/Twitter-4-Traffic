5690dd7fc4fe630af5face46a9ffedfc7ed32224
John Flanagan <johnflan@gmail.com>
2012-01-13 22:16:44 +0000

    Created startup scripts for tfl and twitter scraping

--- data_acquisition/tfl/startCollection.sh
+++ data_acquisition/tfl/startCollection.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Saving data in ../../../t4t-data, debug output"
-python tfltrafficfeedreader.py -d ../../../t4t-data/ -v 10
+echo "Saving data in ../../../t4t-data/tfl, debug output"
+python tfltrafficfeedreader.py -d ../../../t4t-data/tfl/ -v 10
