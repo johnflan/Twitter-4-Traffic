6bcf0e7835ef298b1e631e0166d15b719fbbde58
John Flanagan <johnflan@gmail.com>
2012-01-25 20:21:15 +0000

    Created Android main and test projects

---
+++ mobile_client/t4t/src/uk/ac/ic/doc/t4t/Twitter4trafficActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.ic.doc.t4t;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Twitter4trafficActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
