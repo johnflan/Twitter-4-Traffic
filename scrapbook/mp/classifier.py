e744baa6ca6240b3ac8734ba2790a333a447ae54
mar <mar.polatoglou@gmail.com>
2012-02-08 11:39:20 +0000

    Added the options to read from arguments

--- scrapbook/mp/classifier.py
+++ scrapbook/mp/classifier.py
@@ -24,13 +24,38 @@ def showTweets(**db):
 	    print "Unable to connect to the database"
 
 if __name__ == "__main__":
-    configSection = "Local database"
-    Config = ConfigParser.ConfigParser()
-    Config.read("t4t_credentials.txt")
-    user = Config.get(configSection, "username")
-    password = Config.get(configSection, "password")
-    database = Config.get(configSection, "database")
-    host = Config.get(configSection, "server")
+	configSection = "Local database"
+	Config = ConfigParser.ConfigParser()
+	Config.read("t4t_credentials.txt")
+	user = Config.get(configSection, "username")
+	password = Config.get(configSection, "password")
+	database = Config.get(configSection, "database")
+	host = Config.get(configSection, "server")
 
+	# Parse options from the command line
+	parser = optparse.OptionParser("usage: %prog [options] [action] [tables]")
+	parser.add_option('-H','--host',
+					dest='host',
+					default=host,
+					help='The hostname of the DB')
+	parser.add_option('-d','--database',
+					dest='database',
+					default=database,
+					help='The name of the DB')
+	parser.add_option('-U','--user',
+					dest='user',
+					default=user,
+					help='The username for the DB')
+	parser.add_option('-p','--password',
+					dest='password',
+					default=password,
+					help='The password for the DB')
 
+	(options, args) = parser.parse_args()
 
+	db = dict([[k,v] for k,v in options.__dict__.iteritems() if not v is None ])
+	# Needs an argument (setup, remove or drop)
+	if len(args)==0:
+		sys.exit("Wrong number of arguments")
+
+	sys.exit(main(args[0], *args, **db))
