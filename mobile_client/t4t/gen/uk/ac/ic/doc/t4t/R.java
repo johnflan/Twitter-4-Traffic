b355b7eea98bcd6b6a13d8cccf4b00c9388c9071
John Flanangan <johnflan@gmail.com>
2012-02-17 16:53:35 +0000

    Android: Created app header widget
    Created a header widget for use across all application activities.

--- mobile_client/t4t/gen/uk/ac/ic/doc/t4t/R.java
+++ mobile_client/t4t/gen/uk/ac/ic/doc/t4t/R.java
@@ -15,17 +15,25 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash_bg=0x7f020001;
+        public static final int header_bg=0x7f020000;
+        public static final int header_logo=0x7f020001;
+        public static final int header_share_button=0x7f020002;
+        public static final int header_share_button_on=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int share_button=0x7f020005;
+        public static final int splash_bg=0x7f020006;
     }
     public static final class id {
-        public static final int seekBar1=0x7f060001;
-        public static final int spinner1=0x7f060000;
-        public static final int splash_bg=0x7f060002;
+        public static final int header_share_button=0x7f060002;
+        public static final int imageView1=0x7f060001;
+        public static final int linearLayout1=0x7f060000;
+        public static final int newsList=0x7f060003;
+        public static final int splash_bg=0x7f060004;
     }
     public static final class layout {
-        public static final int eventlist=0x7f030000;
-        public static final int splash=0x7f030001;
+        public static final int app_header=0x7f030000;
+        public static final int eventlist=0x7f030001;
+        public static final int splash=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
