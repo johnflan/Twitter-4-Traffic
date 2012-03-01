89cd68ec6608711916996a5faaa256e81879a3cb
Afxentios Hadjiminas <afxentios.hadjiminas@gmail.com>
2012-03-01 01:57:35 +0000

    Added option for skipping the evaluation

--- scrapbook/ah/nb_bag_of_words.py
+++ scrapbook/ah/nb_bag_of_words.py
@@ -6,8 +6,8 @@ import sys
 import optparse
 import ConfigParser
 import nltk
-import collections
 from preprocessor import preprocessor
+from evaluation import evaluation
 from pg8000 import DBAPI
 
 def main(*args,**opts):
@@ -19,13 +19,15 @@ def main(*args,**opts):
 	
 	# The tweet that need to be classified
 	test_tweet = opts['text']
+	
+	# Enable for testin the data
+	enable_evaluation = opts['test']
 
 	cursor,conn = connectDB(**db)
 
 	#Train and test the classifier
-	classifier = trainClassifier(conn, cursor, tablename, test_tweet)
+	classifier = trainClassifier(conn, cursor, tablename, test_tweet, enable_evaluation)
 
-	#TO DO:: Save the classifier in a file (.pkl) so to use later. http://docs.python.org/library/shelve.html#module-shelve	
 	#TO DO:: Implement cross-validation and ROC Cur from PyML
 
 def connectDB(**db):
@@ -66,7 +68,7 @@ def dump_classifier(classifier, file_name):
 	pickle.dump(classifier, f)
 	f.close()
 	
-def trainClassifier(conn, cursor, tablename, test_tweet):
+def trainClassifier(conn, cursor, tablename, test_tweet, enable_evaluation):
 	"""Train the Naive Bayes"""
 	
 	stop_words = []
@@ -97,7 +99,7 @@ def trainClassifier(conn, cursor, tablename, test_tweet):
 	
 	# Fetch all the non-traffic tweets	
 	try:
-		query_nt = "SELECT tweet FROM "+ tablename +" WHERE ntraffic='y' ORDER BY tid ASC LIMIT 10875"
+		query_nt = "SELECT tweet FROM "+ tablename +" WHERE ntraffic='y' ORDER BY tid ASC LIMIT 681"
 		cursor.execute(query_nt)
 		nttweets = cursor.fetchall()
 	except:
@@ -106,28 +108,29 @@ def trainClassifier(conn, cursor, tablename, test_tweet):
 		print "Select Error -> %s" % exceptionValue
 		lastid="0"
 	
-	
-	# Fetch all the traffic tweets
-	try:
-		query_pt = "SELECT tweet FROM "+ tablename +" WHERE ptraffic='y' ORDER BY tid DESC LIMIT 375"
-		cursor.execute(query_pt)
-		ttweets_test = cursor.fetchall()
-	except:
-		# Get the most recent exception
-		exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-		print "Select Error -> %s" % exceptionValue
-		lastid="0"
-	
-	# Fetch all the non-traffic tweets	
-	try:
-		query_nt = "SELECT tweet FROM "+ tablename +" WHERE ntraffic='y' ORDER BY tid DESC LIMIT 3625"
-		cursor.execute(query_nt)
-		nttweets_test = cursor.fetchall()
-	except:
-		# Get the most recent exception
-		exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-		print "Select Error -> %s" % exceptionValue
-		lastid="0"
+	# If the user chose to evaluate the classifier fetach more labelled tweets for testing
+	if enable_evaluation == 'test':
+		# Fetch all the traffic tweets for the evaluation
+		try:
+			query_pt = "SELECT tweet FROM "+ tablename +" WHERE ptraffic='y' ORDER BY tid DESC LIMIT 375"
+			cursor.execute(query_pt)
+			ttweets_test = cursor.fetchall()
+		except:
+			# Get the most recent exception
+			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+			print "Select Error -> %s" % exceptionValue
+			lastid="0"
+		
+		# Fetch all the non-traffic tweets for the evaluation
+		try:
+			query_nt = "SELECT tweet FROM "+ tablename +" WHERE ntraffic='y' ORDER BY tid DESC LIMIT 375"
+			cursor.execute(query_nt)
+			nttweets_test = cursor.fetchall()
+		except:
+			# Get the most recent exception
+			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+			print "Select Error -> %s" % exceptionValue
+			lastid="0"
 		
 		
 	try:
@@ -161,28 +164,31 @@ def trainClassifier(conn, cursor, tablename, test_tweet):
 		# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 		# >>>>>>>>>>>>>>>>>>>>>>>>>> TEST SET <<<<<<<<<<<<<<<<<<<<<<<<<<<
 		# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-		# Apply preprocessing on the traffic tweets for the test set
-		data=[]
-		for text in ttweets_test:
-			temp = preprocessor().preprocess(text[0],stop_words)
-			data.append(temp)
-		traffic_tweets_test=add_label(data, 'traffic')
 		
-		# Apply preprocessing on the non-traffic tweets for the test set
-		data=[]
-		for text in nttweets_test:
-			temp = preprocessor().preprocess(text[0],stop_words)
-			data.append(temp)
-		nontraffic_tweets_test=add_label(data, 'nontraffic')
-		
-		# Merge the tweets for the test set
-		combined_tweets_test = traffic_tweets_test + nontraffic_tweets_test
-		
-		# Extract the features for the test set
-		temp = []
-		for i in range(len(combined_tweets_test)):
-			temp.append(((features_extractor(combined_tweets_test[i][0])),combined_tweets_test[i][1]))
-		test_set=temp
+		# If the user chose to evaluate the classifier create a test_set
+		if enable_evaluation == 'test':
+			# Apply preprocessing on the traffic tweets for the test set
+			data=[]
+			for text in ttweets_test:
+				temp = preprocessor().preprocess(text[0],stop_words)
+				data.append(temp)
+			traffic_tweets_test=add_label(data, 'traffic')
+			
+			# Apply preprocessing on the non-traffic tweets for the test set
+			data=[]
+			for text in nttweets_test:
+				temp = preprocessor().preprocess(text[0],stop_words)
+				data.append(temp)
+			nontraffic_tweets_test=add_label(data, 'nontraffic')
+			
+			# Merge the tweets for the test set
+			combined_tweets_test = traffic_tweets_test + nontraffic_tweets_test
+			
+			# Extract the features for the test set
+			temp = []
+			for i in range(len(combined_tweets_test)):
+				temp.append(((features_extractor(combined_tweets_test[i][0])),combined_tweets_test[i][1]))
+			test_set=temp
 		
 		# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 		# >>>>>>>>>>>>>>>>>>>>>>>>>> TRAIN THE CLASSIFIER <<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -199,46 +205,17 @@ def trainClassifier(conn, cursor, tablename, test_tweet):
 		# Classify the tweet
 		test_tweet1 = preprocessor().preprocess(test_tweet,stop_words)
 		test = features_extractor(test_tweet1)
-		print "\nThe tweet '%s' is about: %s \n" % (test_tweet1, classifier.classify(test))
+		proba = classifier.prob_classify(test)
+		print "\nThe tweet '%s' is about: %s with probability: %s\n" % (test_tweet, classifier.classify(test),proba.prob('traffic'))
 		
 		# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 		# >>>>>>>>>>>>>>>>>>>>>>>>>> TEST THE CLASSIFIER <<<<<<<<<<<<<<<<<<<<<<<<<<<
 		# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 		
-		referenceSet = collections.defaultdict(set)
-		testSet = collections.defaultdict(set)
-		referenceSet_cm = []
-		testSet_cm = []
-		
-		for index, (tweets, actualLabel) in enumerate(test_set):
-			referenceSet[actualLabel].add(index)
-			referenceSet_cm.append(actualLabel)
-			predictedLabel = classifier.classify(tweets)
-			testSet[predictedLabel].add(index)
-			testSet_cm.append(predictedLabel)
-
-		# Evaluation of the classification
-		# Accuracy is the percentage of the correct classifications of the test_test (fraction of the labelled data)
-		# Recall describes the completeness of the retrieval. It is defined as the portion of the positive examples retrieved by the process versus the  
-		# total  number of existing positive examples (including the ones not retrieved by the process). 
-		# Precision describes the actual accuracy of the retrieval, and is defined as the portion of the positive examples that exist in the total number of 
-		# examples retrieved.
-		
-		print 'Accuracy of the classifier:  ', nltk.classify.util.accuracy(classifier, test_set)
-		print '\nTraffic precision:           ', nltk.metrics.precision(referenceSet['traffic'], testSet['traffic'])
-		print 'Traffic recall:              ', nltk.metrics.recall(referenceSet['traffic'], testSet['traffic'])
-		print 'Traffic F-measure:           ', nltk.metrics.f_measure(referenceSet['traffic'], testSet['traffic'])
-		print '\nNon-Traffic precision:       ', nltk.metrics.precision(referenceSet['nontraffic'], testSet['nontraffic'])
-		print 'Non-Traffic recall:          ', nltk.metrics.recall(referenceSet['nontraffic'], testSet['nontraffic'])
-		print 'Non-Traffic F-measure:       ', nltk.metrics.f_measure(referenceSet['nontraffic'], testSet['nontraffic'])
-		print "\n"
-		
-		# Find the Confusion Matrix for the test set
-		cm = nltk.ConfusionMatrix(referenceSet_cm, testSet_cm)
-		print cm.pp(sort_by_count=True, show_percents=True, truncate=9) 
-		
-		# Show the 10 features with the greatest gain
-		# classifier.show_most_informative_features()
+		# If the user chose to evaluate the classifier apply the evaluation techniques
+		if enable_evaluation == 'test':
+			evaluation(test_set,classifier)
+	
 		
 	except:	
 		# Get the most recent exception
@@ -279,6 +256,10 @@ if __name__ == "__main__":
                     dest='text',
                     default="text",
                     help='Text to be classified')
+    parser.add_option('-t','--test',
+                    dest='test',
+                    default='no',
+                    help='True for evaluation')
 
     (options, args) = parser.parse_args()
 
