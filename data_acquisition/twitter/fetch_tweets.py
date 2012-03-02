03e45f2324237b487ee96581fc81ec4e4f2466fb
Afxentios Hadjiminas <afxentios.hadjiminas@gmail.com>
2012-03-02 20:49:24 +0000

    Now the news tweets are being classified

--- data_acquisition/twitter/fetch_tweets.py
+++ data_acquisition/twitter/fetch_tweets.py
@@ -20,10 +20,9 @@ from datetime import datetime
 DATETIME_STRING_FORMAT = '%a, %d %b %Y %H:%M:%S +0000'
 DATETIME_STAMP_STRING_FORMAT = '%Y%m%d%H%M%S'
 from logging import FATAL, ERROR, WARNING, INFO, DEBUG
-import nltk.data
+import pickle
 from classifier_files.preprocessor import preprocessor
 
-#TO DO:: Create table for the newly classified tweets with additional column for the label and the probability
 
 if __name__ == '__main__':
     import sys
@@ -31,30 +30,7 @@ if __name__ == '__main__':
 
 from crawler import GetRateLimiter, SuppressedCallException
 
-def robust_execute(conn,cursor,query,values=None, maxtries=5):
-    success = False
-    tries = 0
-    while True:
-        try:
-            if values is None:
-                cursor.execute(query)
-            else:
-                cursor.execute(query,values)
-            conn.commit()
-            success = True
-        except OperationalError, oe:
-            print "Query Failed: ", query, values
-            print "Error: ", oe
-            if tries < maxtries:
-                tries += 1
-                print "Retrying..."
-            else:
-                raise
-        if success == True:
-            break
-
-
-def geolondon(  conn,
+def tweets(  conn,
                 rl,
                 georadius="19.622mi",
                 start_id=0,
@@ -64,6 +40,9 @@ def geolondon(  conn,
     most_recent_id = start_id
     geocount = 0
     retweetRegex = re.compile('/\brt\b/i')
+    
+    #>>>>>>>>>>LOAD THE CLASSIFIER<<<<<<<<<<<<<
+    classifier = pickle.load(open("naive_bayes.pickle"))
     
     while True:
         since_id = most_recent_id
@@ -77,33 +56,20 @@ def geolondon(  conn,
                 print "georadius", georadius
                 searchargs = dict(geocode=(51.5,-0.15,georadius),per_page=100,page=page,since_id=since_id,term=term)
                 print "[twitter] rl.FreqLimitGetSearch - %r" % searchargs
+                
                 #GET RESULTS 
                 results = rl.FreqLimitGetSearch(**searchargs)
-				
-				#>>>>>>>>>>LOAD THE CLASSIFIER<<<<<<<<<<<<<
-				classifier = nltk.data.load("/srv/t4t/classifier_files/naive_bayes.pickle")
-				
+                
                 for r in results:
                     most_recent_id = max(r.id,most_recent_id)
                     (tid,uname,created_at_str,location,text,geo) = r.id,r.user.screen_name,r.created_at,r.location,r.text,r.GetGeo()
                     created_at = datetime.strptime(created_at_str, DATETIME_STRING_FORMAT)
 					
                     text = text.encode('ascii','ignore')
-					
-					#>>>>>>>>>>>>>>>>>>CLASSIFY THE TWEET<<<<<<<<<<<<<<<<<<<<<<<
-					pro_text = preprocessor().preprocess(text,[])
-					ft_pro_text = features_extractor(pro_text)
-					label = classifier.classify(ft_pro_text)
-					
-					#>>>>>>>>>>>>>>>>>>>>>FIND THE PROBABILITY<<<<<<<<<<<<<<<<<<
-					if label == 'traffic':
-						probability_dict  = classifier.prob_classify(ft_pro_text)
-						probability = probability_dict.prob('traffic')
-					elif label == 'nontraffic':
-						probability_dict  = classifier.prob_classify(ft_pro_text)
-						probability = probability_dict.prob('nontraffic')
-					
-					
+                    
+                    #>>>>>>>>>>>>>>>>>>CLASSIFY THE TWEET<<<<<<<<<<<<<<<<<<<<<<<
+                    isTraffic, probability = classify_traffic(text,classifier)    
+
                     now = datetime.now()
                     nowstr = datetime.strftime(now, DATETIME_STRING_FORMAT)
                     #print (tid,uname,created_at,location,text,geo)
@@ -111,9 +77,9 @@ def geolondon(  conn,
                     # If the tweet is a retweet drop it, this method is not
                     # 100%. But the API is not returning if r.retweet
                     isRetweet = re.search('RT\s@', r.text, re.IGNORECASE)
-                    if isRetweet:
+                    if isRetweet or not isTraffic:
                         continue
-
+                    print "Found one"
                     # geo part
                     if not geo is None and geo.get('type') == 'Point':
                         geolat,geolong, = geo['coordinates']
@@ -123,15 +89,15 @@ def geolondon(  conn,
 
                         text = text.replace("'", "")
                         text = text.replace("%", "")
-                        query = "INSERT INTO geolondon(tid, uname, created_at,\
-                        location,text,geolocation) VALUES (" + str(tid) +\
+                        query = "INSERT INTO tweets(tid, uname, created_at,\
+                        location,text,geolocation,probability) VALUES (" + str(tid) +\
                         ",'" + uname + "', to_timestamp('" + str(created_at) + "','YYYY-MM-DD HH24:MI:SS'),'" +\
                         str(location) + "',\'" + str(text) +\
-                        "\'," + geoloc + ")"
+                        "\'," + geoloc + "," + str(probability) + ")"
 
                         print query
                         print uname, "\t", text[:50], geo['coordinates']
-                        #query = """INSERT INTO geolondon(tid, uname,
+                        #query = """INSERT INTO tweets(tid, uname,
                         #created_at, location, text, geolocation)
                         #      VALUES(%s,%s,to_timestamp(%s, \'YYYY-MM-DD
                         #      HH24:MI:SS\'),%s,%s,ST_GeographyFromText(SRID=4326;POINT(%s,%s)))"""
@@ -145,9 +111,9 @@ def geolondon(  conn,
                             continue
 
                     else:
-                        sql_query = """INSERT INTO geolondon(tid, uname, created_at, location, text) VALUES
-                                    (%s,%s,to_timestamp(%s, \'YYYY-MM-DD HH24:MI:SS\'),%s,%s)"""
-                        params = (tid, uname, str(created_at), location, text)
+                        sql_query = """INSERT INTO tweets(tid, uname, created_at, location, text, probability ) VALUES
+                                    (%s,%s,to_timestamp(%s, \'YYYY-MM-DD HH24:MI:SS\'),%s,%s,%s)"""
+                        params = (tid, uname, str(created_at), location, text, probability)
                         #print "[params]", params
                         print uname, "\t", text[:50]
                         try:
@@ -176,48 +142,11 @@ def loadSearchTerms(kwargs):
         print "[Error] search terms file not found"
         sys.exit()
 
-def createTables(conn, cursor):
-    #REMOVE THIS LINE!!!!
-    print "[debug] droping tables to begin - THIS MUST BE REMOVED"
-    cursor.execute("DROP TABLE geolondon, options");
-    conn.commit()
-    
-    #Postgres does not support CREATE TABLE IF *NOT*
-    #a solution seems to be just create the table
-    #if it already exists nothing happens
-    #query = 'CREATE TABLE IF NOT EXISTS geolondon(tid INTEGER PRIMARY KEY,uname VARCHAR(40), created_at DATETIME,location VARCHAR(128), text VARCHAR(150), geolat FLOAT, geolong FLOAT )'
-    query = """CREATE TABLE geolondon(tid BIGINT PRIMARY KEY,uname VARCHAR(40),
-    created_at TIMESTAMP,location VARCHAR(128), text VARCHAR(200), geolocation GEOGRAPHY(POINT, 4326) )"""
-    params = None
-    print "[sql] cursor.execute( %s , %r) " % ( query , params )
-    cursor.execute(query)
-    conn.commit()
-    #cursor.execute('CREATE INDEX IF NOT EXISTS geolon_tid_idx ON geolondon(tid)')
-    cursor.execute('CREATE INDEX geolon_tid_idx ON geolondon(tid)')
-    conn.commit()
-
-    #query = 'CREATE TABLE IF NOT EXISTS options (timestamp DATETIME, key TEXT, value TEXT, UNIQUE(timestamp, key))'
-    query = 'CREATE TABLE options (timestamp TIMESTAMP, key TEXT, value TEXT, UNIQUE(timestamp, key))'
-    print "[sql] cursor.execute( %s) " % query
-    cursor.execute(query)
-    conn.commit()
-
-    timestamp = datetime.now()
-    #postgres needs this insert format for timestamps
-    #to_timestamp('2012-01-26 01:09:26.721779', 'YYYY-MM-DD HH:MI:SS.US')
-    for key,value in kwargs.iteritems():
-        query = 'INSERT INTO options VALUES(to_timestamp(\'' + str(timestamp) \
-                + '\' ,\'YYYY-MM-DD HH24:MI:SS.US\'), \'' + key + '\' , \'' \
-                + str(value) + '\')'
-        print "[sql]", query
-        cursor.execute(query)
-    conn.commit()
-
 
 def main(*args,**kwargs):
     configSection = "Local database"
     Config = ConfigParser.ConfigParser()
-    Config.read("../t4t_credentials.txt")
+    Config.read("t4t_credentials.txt")
     cfg_username = Config.get(configSection, "username")
     cfg_password = Config.get(configSection, "password")
     cfg_database = Config.get(configSection, "database")
@@ -238,13 +167,6 @@ def main(*args,**kwargs):
             user=cfg_username, password=cfg_password)
     cursor = conn.cursor()
 
-    if kwargs['reset-db']:
-        print "[INFO] Reset DB, this cannot be undone - are you sure? y/n"
-        inputToken = raw_input('> ')
-        if inputToken == 'y':
-            createTables(conn, cursor)
-            print "done."
-
     if kwargs['query_max_id']:
         print get_max_id(conn,cursor)
         return
@@ -258,7 +180,7 @@ def main(*args,**kwargs):
 
     rl = GetRateLimiter()
     rl.api._cache_timeout = 30
-    geolondon(conn,rl,**kwargs)
+    tweets(conn,rl,**kwargs)
 
 
 def get_max_id(conn,cursor):
@@ -266,6 +188,21 @@ def get_max_id(conn,cursor):
     cursor.execute(query)
     ((start_id,),) = cursor.fetchall()
     return start_id
+    
+def classify_traffic(text,classifier):
+    pro_text = preprocessor().preprocess(text,[])
+    ft_pro_text = dict([(word, True) for word in pro_text])
+    label = classifier.classify(ft_pro_text)
+    isTraffic = False
+    probability = 0
+    #>>>>>>>>>>>>>>>>>>>>>FIND THE PROBABILITY<<<<<<<<<<<<<<<<<
+    if label == 'traffic':
+        isTraffic = True 
+        probability_dict  = classifier.prob_classify(ft_pro_text)
+        probability = probability_dict.prob('traffic')
+		
+	print text, probability, isTraffic
+    return isTraffic, probability
 
 if __name__ == '__main__':
     import StringIO
@@ -289,11 +226,7 @@ if __name__ == '__main__':
                         dest='query_max_id', 
                         default=False, 
                         action ='store_true',
-                        help='Simply return the maximum id currently in the table')
-    parser.add_option('--reset-db',
-                        dest='reset-db',
-                        default=False,
-                        help='Wipe the current database tables')
+                        help='Simply return the maximum id currently in the table')    
     (options, args) = parser.parse_args()
     kwargs = dict([[k,v] for k,v in options.__dict__.iteritems() if not v is None ])
     main(*args,**kwargs)
